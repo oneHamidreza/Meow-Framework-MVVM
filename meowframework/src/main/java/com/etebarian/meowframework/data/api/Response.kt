1bca1002fd56f0adf335a2ca4e4986051062efad
Hamidreza Etebarian <shetmobile@gmail.com>
2020-02-24 01:28:36 +0330

    Response.kt & NetworkConnectionException.kt & MeowRepository.kt added

---
+++ meowframework/src/main/java/com/etebarian/meowframework/data/api/Response.kt
@@ -0,0 +1,50 @@
+package com.etebarian.meowframework.data.api
+
+/**
+ * The model of Response in restful api by success and fail statuses.
+ * @author  Hamidreza Etebarian
+ * @version 1.0.0
+ * @since   2020-02-24
+ */
+
+/**
+ * The parent of restful api responses.
+ * @property T containing the type of embedded class in Response
+ */
+sealed class Response<out T> {
+
+    /**
+     * Success response model in restful api.
+     * @property T containing the type of embedded class in Response
+     * @property data containing the received data from a restful api at success status.
+     */
+    class Success<out T>(val data: T) : Response<T>()
+
+    /**
+     * Success Response in restful api.
+     * @property exception containing the exception at fail status.
+     * @property code containing the http response code.
+     * @property error containing the error has been exist or not.
+     * @property errors containing the errorX items if has been exists.
+     * @property message containing the http message.
+     * @property method containing the http method.
+     * @property path containing the http path.
+     */
+    data class Error(val exception: Throwable,
+                     val code: Int? = null,
+                     val error: Boolean? = null,
+                     val errors: List<ErrorX>? = null,
+                     val message: String? = null,
+                     val method: String? = null,
+                     val path: String? = null) : Response<Nothing>()
+}
+
+/**
+ * Error Response in restful api.
+ * @property message containing the errror message .
+ * @property path containing the http path.
+ */
+data class ErrorX(
+        val message: String,
+        val path: String
+)
---
+++ meowframework/src/main/java/com/etebarian/meowframework/data/api/exceptions/NetworkConnectionException.kt
@@ -0,0 +1,18 @@
+package com.etebarian.meowframework.data.api.exceptions
+
+/**
+ * A exception class for network connection issue such as NO WIFI or NO CELUAR NETWORK and etc
+ * and containing no internet connection statuses.
+ * @author  Hamidreza Etebarian
+ * @version 1.0.0
+ * @since   2020-02-23
+ */
+
+
+/**
+ * NetworkConnectionException class.
+ * @author  Hamidreza Etebarian
+ * @version 1.0.0
+ * @property message containing the string of message exception.
+ */
+class NetworkConnectionException(message: String? = "Internet connection required.") : Exception(message)
