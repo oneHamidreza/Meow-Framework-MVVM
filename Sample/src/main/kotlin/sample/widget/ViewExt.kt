982d2faa271d83b98f158e9fd445837c674a1aa4
oneHamidreza <oneHamidreza@gmail.com>
2020-05-02 00:58:39 +0430

    Markwon now supports HTML.

--- Sample/src/main/kotlin/sample/widget/ViewExt.kt
+++ Sample/src/main/kotlin/sample/widget/ViewExt.kt
@@ -22,6 +22,7 @@ import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import io.noties.markwon.Markwon
 import io.noties.markwon.ext.tables.TablePlugin
+import io.noties.markwon.html.HtmlPlugin
 import io.noties.markwon.image.ImagesPlugin
 import io.noties.markwon.syntax.Prism4jThemeDarkula
 import io.noties.markwon.syntax.Prism4jThemeDefault
@@ -72,6 +73,7 @@ fun TextView.setMarkdownData(markdownData: String?) {
         val highlight = SyntaxHighlightPlugin.create(prism4j, theme)
         usePlugin(highlight)
 
+        usePlugin(HtmlPlugin.create())
         usePlugin(ImagesPlugin.create())
         usePlugin(TablePlugin.create(context))
     }.build().setMarkdown(this, markdownData)
