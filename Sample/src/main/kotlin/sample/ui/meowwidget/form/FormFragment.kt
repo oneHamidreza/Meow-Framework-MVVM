828af8d81f94918289ac68a4f8784d1edf780e1f
Ali <s.a.modares.h@gmail.com>
2020-05-03 00:46:26 +0430

    Material Date Picker was added

--- Sample/src/main/kotlin/sample/ui/meowwidget/form/FormFragment.kt
+++ Sample/src/main/kotlin/sample/ui/meowwidget/form/FormFragment.kt
@@ -22,11 +22,14 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.ArrayAdapter
+import com.google.android.material.datepicker.MaterialDatePicker
 import meow.ktx.instanceViewModel
 import meow.ktx.toastL
 import sample.R
 import sample.databinding.FragmentFormBinding
 import sample.ui.base.BaseFragment
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Material Form Fragment.
@@ -41,7 +44,7 @@ class FormFragment : BaseFragment<FragmentFormBinding>() {
     private val viewModel: FormViewModel by instanceViewModel()
     override fun layoutId() = R.layout.fragment_form
 
-    @SuppressLint("PrivateResource")
+    @SuppressLint("PrivateResource", "SimpleDateFormat")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -92,6 +95,32 @@ class FormFragment : BaseFragment<FragmentFormBinding>() {
         }
 
         binding.pv.error = getString(R.string.error_sample)
+
+        binding.hintButton.setOnClickListener {
+            val builder = MaterialDatePicker.Builder.datePicker()
+            builder.setTitleText("Please Choose your date")
+            val picker = builder.build()
+            picker.addOnPositiveButtonClickListener { selection ->
+
+                val simpleFormat = SimpleDateFormat("MM/dd/yyyy")
+                binding.hintButton.titleText = simpleFormat.format(Date(selection))
+
+//                Date and Time Pattern                Result
+//                -----------------------------        ---------------------------------
+//                "yyyy.MM.dd G 'at' HH:mm:ss z"       2001.07.04 AD at 12:08:56 PDT
+//                "EEE, MMM d, ''yy"                   Wed, Jul 4, '01
+//                "h:mm a"                             12:08 PM
+//                "hh 'o''clock' a, zzzz"              12 o'clock PM, Pacific Daylight Time
+//                "K:mm a, z"                          0:08 PM, PDT
+//                "yyyyy.MMMMM.dd GGG hh:mm aaa"       02001.July.04 AD 12:08 PM
+//                "EEE, d MMM yyyy HH:mm:ss Z"         Wed, 4 Jul 2001 12:08:56 -0700
+//                "yyMMddHHmmssZ"                      010704120856-0700
+//                "yyyy-MM-dd'T'HH:mm:ss.SSSZ"         2001-07-04T12:08:56.235-0700
+//                "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"       2001-07-04T12:08:56.235-07:00
+//                "YYYY-'W'ww-u"                       2001-W27-3
+            }
+            picker.show(parentFragmentManager, "date_picker_tag")
+        }
     }
 
     override fun initViewModel() {
